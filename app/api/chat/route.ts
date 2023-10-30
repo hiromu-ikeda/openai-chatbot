732e71173f1a7dfbcc3cea04cf80e64ae507fffd
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-10-30 15:53:13 +0900

    一つずつデバッグ

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,3 @@
-import { readFile } from "fs/promises"
-import { Document, VectorStoreIndex } from "llamaindex"
 import { NextResponse } from "next/server"
 
 export const runtime = "nodejs"
@@ -8,17 +6,19 @@ export const POST = async (req: Request) => {
   const { message } = await req.json()
   console.log(message)
 
-  const kouhaku = await readFile("public/kouhaku.csv", "utf-8")
-  console.log(kouhaku)
+  // const kouhaku = await readFile("public/kouhaku.csv", "utf-8")
+  // console.log(kouhaku)
 
-  const document = new Document({ text: kouhaku })
+  // const document = new Document({ text: kouhaku })
 
-  const index = await VectorStoreIndex.fromDocuments([document])
+  // const index = await VectorStoreIndex.fromDocuments([document])
 
-  const queryEngine = index.asQueryEngine()
+  // const queryEngine = index.asQueryEngine()
 
-  const response = await queryEngine.query(message)
-  console.log(response.toString())
+  // const response = await queryEngine.query(message)
+  // console.log(response.toString())
 
-  return NextResponse.json(response.toString())
+  // return NextResponse.json(response.toString())
+
+  return NextResponse.json({ message: "Hello World" })
 }
